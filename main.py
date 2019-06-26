837f1fce90e43962d80f247516959fad8abe4c63
GeorgeAlanis <georgesaaurio@gmail.com>
2019-06-26 10:58:09 -0500

    Huge fix on every file

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import utils as utl
 import pso
 
 data = utl.getData(file_path="assets/sprint7ToroideMixto.csv")
-pso = pso.PSO(data, 10, max_iter=100, use_var=False)
-print(pso.search())
+pso = pso.PSO(data, 10, max_iter=100, use_var=True)
+print(pso.search)
--- pso.py
+++ pso.py
@@ -1,120 +1,117 @@
-import copy
-
 import numpy as np
-import pandas as pd
-from numpy.random import ranf
+import utils
+import classes
+from copy import deepcopy
 from sklearn.cluster import KMeans
 from tqdm import tqdm
 
-import classes
-import utils
-
 
 class Particle:
-    def __init__(self, clusters, use_variance=True):
-        self._clusters = copy.deepcopy(clusters)
-        self.centroids = []
-        self.store_times = []
-        self.vel = np.zeros([len(clusters), 2])
-        self.use_variance = use_variance
-        self.fitness = float('-inf')
+    def __init__(self, clusters, elements, fitness_by='variance'):
+        self.number_of_clusters = len(clusters)
+        self.fitness = float("-inf")
+        self.centroids = np.zeros([self.number_of_clusters, 2])
+        self.areas = np.zeros(self.number_of_clusters)
+        self.store_times = np.zeros(self.number_of_clusters)
+        self.clusters = deepcopy(clusters)
+        self.elements = elements.copy()
+        self.number_of_elements = len(self.elements)
+        self.vel = np.zeros([self.number_of_clusters, 2])
+        self.fitness_by = fitness_by
 
-        for clusterPdV in clusters:
-            self.centroids.append(clusterPdV.centroid)
-            self.store_times.append(clusterPdV.total_time)
+        for i in xrange(self.number_of_clusters):
+            self.centroids[i] = clusters[i].centroid
+            self.store_times[i] = clusters[i].total_time
+            self.areas[i] = clusters[i].area
 
-        self.best_centroids = copy.deepcopy(self.centroids)
+        self.calculate_fitness()
         self.best_fitness = self.fitness
+        self.best_centroids = self.centroids.copy()
 
     def calculate_fitness(self):
-        if self.use_variance:
+        if self.fitness_by == 'variance':
             self.fitness = -1.0 * np.var(self.store_times)
-        else:
-            self.fitness = -1.0 * np.var([cluster.area() for cluster in self._clusters])
 
-    def move(self, W, C1, C2, Gb):
-        R1, R2 = ranf(size=2)
+    def move(self, w, c1, c2, global_best_centroids):
+        r1, r2 = np.random.ranf(size=2)
 
-        for i in xrange(len(self.centroids)):
-            self.vel[i] = (W * self.vel[i]) + (C1 * R1 * (utils.euclidean(self.best_centroids[i], self.centroids[i])))\
-                          + (C2 * R2 * (utils.euclidean(Gb[i], self.centroids[i])))
+        for i in xrange(self.number_of_clusters):
+            self.vel[i] = (w * self.vel[i]) + (c1 * r1 * (utils.euclidean(self.best_centroids[i], self.centroids[i])))\
+                          + (c2 * r2 * (utils.euclidean(global_best_centroids[i], self.centroids[i])))
             self.centroids[i] += self.vel[i]
 
         self.recluster()
-
         self.calculate_fitness()
 
         if self.fitness > self.best_fitness:
             self.best_fitness = self.fitness
-            self.best_centroids = self.centroids[:]
+            self.best_centroids = self.centroids.copy()
 
     def recluster(self):
-        for i in xrange(len(self._clusters)):
-            for j in self._clusters[i]._elements:
-                smallest = [float('inf'), None]
-                moved = []
-                for w in xrange(len(self.centroids)):
-                    if utils.euclidean(j, self.centroids[w]) < smallest[0]:
-                        smallest[0] = utils.euclidean(j, self.centroids[w])
-                        smallest[1] = w
-                if smallest[1] != i:
-                    self._clusters[smallest[1]].push_back(j)
-                    moved.append(j)
-            if moved:
-                for m in moved:
-                    self._clusters[i].remove(m)
+        for i in xrange(self.number_of_elements):
+            smallest_distance = [float('inf'), None]
+
+            for j in xrange(self.number_of_clusters):
+                current_distance = utils.euclidean(self.elements[i], self.clusters[j].centroid)
+
+                if current_distance < smallest_distance[0]:
+                    smallest_distance = [current_distance, j]
+
+            if smallest_distance[1] != self.elements[i][-1]:
+                self.clusters[self.elements[i][-1]].remove(self.elements[i])
+                self.clusters[j].push_back(self.elements[i])
 
 
-class PSO:
-    def __init__(self, data, n_particles, seed=42, max_iter=10, W=1, C1=.5, C2=.5, use_var=True):
-        self.Gb_centroids = [float('-inf') for _ in xrange(13)]
-        self.Gb_fit = float("-inf")
-        self.data = data
-        self.n_particles = n_particles
-        self.max_iter = max_iter
-        self.W = W
-        self.C1 = C1
-        self.C2 = C2
+class ClusteringPSO:
+    def __init__(self, data, n_particles, n_clusters=13, seed=42, max_iter=10, w=1, c1=.5, c2=.5, fit_by='variance'):
+        self.data = data.copy()
+        self.number_of_elements = len(self.data)
+        self.number_of_particles = n_particles
+        self.number_of_clusters = n_clusters
         self.seed = np.random.RandomState(seed)
-        self.use_var = use_var
-        self.population = self.generate_population()
+        self.max_iterations = max_iter
+        self.w = w
+        self.c1 = c1
+        self.c2 = c2
+        self.fit_by = fit_by
+        self.population = []
+        self.global_best_centroids = np.zeros([self.number_of_clusters,2])
+        self.global_best_fitness = float("-inf")
 
-    def search(self):
+        self.generate_population()
         self.find_best()
-        with tqdm(total=self.max_iter, unit=' Epoch') as pb:
-            pb.set_postfix(fitness=self.Gb_fit, refresh=False)
-            for _ in xrange(self.max_iter):
-                for particle in self.population:
-                    particle.move(self.W, self.C1, self.C2, self.Gb_centroids)
-                    if particle.fitness > self.Gb_fit:
-                        self.Gb_fit = particle.fitness
-                        self.Gb_centroids = particle.centroids[:]
-                        pb.set_postfix(fitness=self.Gb_fit, refresh=False)
-                pb.update()
-            return self.Gb_fit, self.Gb_centroids
 
-    def generate_population(self, n_clusters=13):
-        population = []
-
-        for _ in tqdm(xrange(self.n_particles), desc='Generando Poblacion inicial', unit=' Particle'):
-            individuo = []
-            cluster = KMeans(n_clusters=n_clusters, random_state=self.seed)
+    def generate_population(self):
+        for _ in tqdm(xrange(self.number_of_particles), desc='Generando Poblacion inicial', unit=' Particle'):
+            elements = self.data.copy()
+            cluster = KMeans(n_clusters=self.number_of_clusters, random_state=self.seed)
             cluster_labels = cluster.fit_predict(self.data[:, 0:2])
-            individuo_df = pd.DataFrame({'x': self.data[:, 0],
-                                         'y': self.data[:, 1],
-                                         'time_store': self.data[:, 2],
-                                         'cluster': cluster_labels})
-            for i in xrange(n_clusters):
-                individuo.append(
-                    classes.ClusterPdV(
-                        [classes.PdV(*point[:-1]) for point in individuo_df[individuo_df.cluster == i].values],
-                        cluster.cluster_centers_[i]))
+            clusters = [classes.ClusterPdv(center, 0.0, 0.0) for center in cluster.cluster_centers_]
 
-            population.append(Particle(individuo, self.use_var))
-        return population
+            for i in xrange(self.number_of_elements):
+                elements[i][-1] = cluster_labels[i]
+                clusters[cluster_labels[i]].push_back(elements[i])
+
+            self.population.append(Particle(clusters, elements, self.fit_by))
 
     def find_best(self):
-        for particle in self.population:
-            if particle.fitness > self.Gb_fit:
-                self.Gb_fit = particle.fitness
-                self.Gb_centroids = particle.centroids
+        for i in xrange(self.number_of_particles):
+            if self.population[i].fitness > self.global_best_fitness:
+                self.global_best_fitness = self.population[i].fitness
+                self.global_best_centroids = self.population[i].centroids.copy()
+
+    @property
+    def search(self):
+        best_particle = None
+        with tqdm(total=self.max_iterations, unit=' Epoch') as pb:
+            pb.set_postfix(fitness=self.global_best_fitness, refresh=True)
+            for _ in xrange(self.max_iterations):
+                for particle in self.population:
+                    particle.move(self.w, self.c1, self.c2, self.global_best_centroids)
+                    if particle.fitness > self.global_best_fitness:
+                        best_particle = deepcopy(particle)
+                        self.global_best_fitness = particle.fitness
+                        self.global_best_centroids = particle.centroids.copy()
+                        pb.set_postfix(fitness=self.Gb_fit, refresh=True)
+                pb.update()
+            return best_particle
